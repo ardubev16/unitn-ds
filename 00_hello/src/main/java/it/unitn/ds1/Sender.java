395e3f5a6d5293c418ee3a41a4b9a16ec031723e
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2025-03-26 11:03:26 +0100

    style: run the formatter I have configured in Vim

--- 00_hello/src/main/java/it/unitn/ds1/Sender.java
+++ 00_hello/src/main/java/it/unitn/ds1/Sender.java
@@ -1,4 +1,5 @@
 package it.unitn.ds1;
+
 import scala.concurrent.duration.Duration;
 import java.util.concurrent.TimeUnit;
 import akka.actor.AbstractActor;
@@ -9,35 +10,34 @@ import it.unitn.ds1.Receiver.Hello;
 
 // The Sender actor
 public class Sender extends AbstractActor {
-  private ActorRef receiver;
+    private ActorRef receiver;
 
-  public Sender(ActorRef receiver) {
-    this.receiver = receiver; // this actor will be the destination of our messages
-  }
-  static public Props props(ActorRef receiverActor) {
-    return Props.create(Sender.class, () -> new Sender(receiverActor));
-  }
+    public Sender(ActorRef receiver) {
+        this.receiver = receiver; // this actor will be the destination of our messages
+    }
 
-  @Override
-  public void preStart() {
+    static public Props props(ActorRef receiverActor) {
+        return Props.create(Sender.class, () -> new Sender(receiverActor));
+    }
 
-    // Create a timer that will periodically send a message to the receiver actor
-    Cancellable timer = getContext().system().scheduler().scheduleWithFixedDelay(
-        Duration.create(1, TimeUnit.SECONDS),               // when to start generating messages
-        Duration.create(1, TimeUnit.SECONDS),               // how frequently generate them
-        receiver,                                           // destination actor reference
-        new Hello("Hello from " + getSelf().path().name()), // the message to send
-        getContext().system().dispatcher(),                 // system dispatcher
-        getSelf()                                           // source of the message (myself)
+    @Override
+    public void preStart() {
+
+        // Create a timer that will periodically send a message to the receiver actor
+        Cancellable timer = getContext().system().scheduler().scheduleWithFixedDelay(
+                Duration.create(1, TimeUnit.SECONDS), // when to start generating messages
+                Duration.create(1, TimeUnit.SECONDS), // how frequently generate them
+                receiver, // destination actor reference
+                new Hello("Hello from " + getSelf().path().name()), // the message to send
+                getContext().system().dispatcher(), // system dispatcher
+                getSelf() // source of the message (myself)
         );
 
+    }
 
-  }
-
-  @Override
-  public Receive createReceive() {
-    return receiveBuilder().build(); // this actor does not handle any incoming messages
-  }
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder().build(); // this actor does not handle any incoming messages
+    }
 
 }
-
